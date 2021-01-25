5a45b206a5610c7da75ec2d9c4b83b102fb9a61b
Abtin Paliz Co <abtinokhovat@gmail.com>
2021-01-25 17:04:59 +0330

    Add project files.

---
+++ APIMethodCreator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMethodCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=server-paliz\sql2017;Initial Catalog=avamarket;User ID=sa;Password=Pa_12345");
+            var cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "dbo.SP_API_DynamicCreate";
+            cmd.Connection = con;
+            var dt = new DataTable();
+            var da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string modelName = dt.Rows[i].ItemArray[2].ToString();
+                string fileName = @"C:\Users\Paliz Co\Desktop\New Folder\" + modelName + ".txt";
+                // Create a new file     
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file    
+                    Byte[] title = new UTF8Encoding(true).GetBytes(dt.Rows[i].ItemArray[3].ToString());
+                    fs.Write(title, 0, title.Length);
+                }
+
+            }
+        }
+
+    }
+}
